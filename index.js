f88c55cd892436a902915032db622f2d706eb811
Rufat Aghayev <rufat.web@gmail.com>
2018-08-09 04:12:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ function nowServing(line) {
   }
 }
 function currentLine(line) {
-  var lineDisplay = "";
+  var lineDisplay = [];
   if (line.length === 0) {
     return "The line is currently empty.";
   } else {
     for (var i = 0; i < line.length; i++) {
       lineDisplay += (i + 1) + "." + line[i] + ", "
     }
-    lineDisplay = lineDisplay.length - 2;
+    lineDisplay = lineDisplay.slice(0, lineDisplay.length - 2);
     return "The line is currently: " + lineDisplay;
 }
