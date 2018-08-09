590c3416800e8f06baa9910f499d041415b859b0
Rufat Aghayev <rufat.web@gmail.com>
2018-08-09 03:29:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ function takeANumber(katzDeliLine, name) {
   return "Welcome, " + name + ". You are number " +  katzDeliLine.length + " in line."
 }
 
-function nowServing(katzDeliLine) {
-  if (katzDeliLine.length === 0) {
+function nowServing(currentLine) {
+  if (currentLine.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
-    return "Currently serving " + katzDeliLine.shift(0) + ".";
+    return "Currently serving " + currentLine.shift(0) + ".";
   }
 }
