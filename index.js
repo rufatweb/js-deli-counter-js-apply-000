fdadd2ccfa807cea521bcae9bd724a1ef0de4198
Rufat Aghayev <rufat.web@gmail.com>
2018-08-09 03:30:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var line = [];
-function takeANumber(katzDeliLine, name) {
+function takeANumber(line, name) {
   line.push(name)
   return "Welcome, " + name + ". You are number " +  line.length + " in line."
 }
